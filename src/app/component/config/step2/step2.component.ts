6645bd06c43deff7f15931745af6de2004989c58
Saltiplouf <polooavecdeuxo@gmail.com>
2023-07-11 16:09:50 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/app/app.component.html
    #	src/app/app.component.scss
    #	src/styles.scss

--- src/app/component/config/step2/step2.component.ts
+++ src/app/component/config/step2/step2.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Store} from "@ngrx/store";
+import {State} from "../../../reducer";
+import {Router} from "@angular/router";
+import {Step1, Step2} from "../../../actions/config.action";
 
 @Component({
   selector: 'app-step2',
@@ -6,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./step2.component.scss']
 })
 export class Step2Component {
+  constructor(private store: Store<State>, private router: Router) {}
 
+  selectColor(Color: string) {
+    this.store.dispatch(Step2({ Color }));
+    this.router.navigate(['step3']);
+  }
 }
