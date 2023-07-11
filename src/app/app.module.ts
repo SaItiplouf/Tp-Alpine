3a6fa51fd4cc1c11fa2e799090fa1f33969186e7
NathanSilv404 <120452369+NathanSilv404@users.noreply.github.com>
2023-07-11 09:33:09 +0200

    push 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VersionComponent } from './version/version.component';
+import { ColorComponent } from './color/color.component';
+import { RimComponent } from './rim/rim.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VersionComponent,
+    ColorComponent,
+    RimComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
