5831b4409634a2c997c685b6b5ff24c872910923
Saltiplouf <polooavecdeuxo@gmail.com>
2023-07-17 10:53:30 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/app/app.component.html
    #	src/app/app.component.scss
    #	src/styles.scss

--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -1,11 +1,13 @@
 import { createReducer, on } from '@ngrx/store';
-import {Step1, Step2, Step3, Step4} from './actions/config.action';
+import { Step1, Step2, Step3, Step4 } from './actions/config.action';
+import DATA from '../app/data.ts';
 
 export interface State {
   selectedCarId: number;
   Color: string;
   Wheels: string;
   Seat: string;
+  Data: typeof DATA;
 }
 
 export const initialState: State = {
@@ -13,8 +15,8 @@ export const initialState: State = {
   Color: "",
   Wheels: "",
   Seat: "",
+  Data: DATA,
 };
-
 export const reducer = createReducer(
   initialState,
   on(Step1, (state, { carId }) => {
